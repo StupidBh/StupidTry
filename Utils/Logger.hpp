348fd3926261139ac54480e8170ba60d95c51b6f
StupidBhh <stupidbhh@gmail.com>
2025-05-24 12:21:56 +0800

    使用异步日志系统

--- Utils/Logger.hpp
+++ Utils/Logger.hpp
@@ -1,39 +1,68 @@
 ﻿#pragma once
 #include "SingletonHolder.hpp"
 
+#include <iostream>
 #include <filesystem>
+#include <mutex>
+#include <shared_mutex>
 
+#include "spdlog/async.h"
 #include "spdlog/spdlog.h"
+#include "spdlog/stopwatch.h"
 #include "spdlog/fmt/ranges.h" // 启用容器格式化支持
-#include "spdlog/sinks/stdout_color_sinks.h"
 #include "spdlog/sinks/daily_file_sink.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
 
 namespace _Logging_ {
     class Logger final : public asst::SingletonHolder<Logger> {
         DELETE_COPY_AND_MOVE(Logger);
 
-        friend class asst::SingletonHolder<Logger>;
-        Logger() = default;
+        Logger()
+        {
+            try {
+                spdlog::init_thread_pool(32768, 2);
+                InitLog(".", TARGET_NAME, false);
+            }
+            catch (const spdlog::spdlog_ex& e) {
+                std::cerr << "Logger initialization failed: " << e.what() << std::endl;
+                std::abort();
+            }
+        }
 
     public:
-        ~Logger() override = default;
+        ~Logger() override { ShutDown(); }
 
-        inline std::unique_ptr<spdlog::logger>& log()
+        inline std::shared_ptr<spdlog::async_logger>& log()
         {
-            if (m_log == nullptr) {
-                this->InitLog(".", TARGET_NAME, false);
-            }
+            std::shared_lock<std::shared_mutex> lock(m_mutex);
             return this->m_log;
         }
 
         void InitLog(const std::string& work_path, const std::string& file_name, bool verbose = false)
         {
-            constexpr auto log_fmt =
+            constexpr const char* log_fmt =
 #ifdef _DEBUG
-                // [年-月-日] [时-分-秒-毫秒] [P:进程ID] [日志等级] [文件名:行号]
-                "[%Y-%m-%d %H:%M:%S.%e] [P:%P] [%^%l%$] [%s:%#] %v";
+                // [年-月-日] [时-分-秒-毫秒] [T:线程ID] [P:进程ID] [日志等级] [文件名:行号]
+                "[%Y-%m-%d %H:%M:%S.%e] [T:%t] [P:%P] [%^%l%$] [%s:%#] %v";
 #else
-                "[%Y-%m-%d %H:%M:%S.%e] [P:%P] [%^%l%$] %v";
+                "[%Y-%m-%d %H:%M:%S.%e] [T:%t] [P:%P] [%^%l%$] %v";
+#endif
+
+            // 创建日志目录
+            std::string log_dir = fmt::format("{}/logs", work_path);
+            try {
+                if (!std::filesystem::exists(log_dir)) {
+                    std::filesystem::create_directories(log_dir);
+                }
+            }
+            catch (const std::filesystem::filesystem_error& e) {
+                std::cerr << "Failed to create log directory: " << e.what() << std::endl;
+                std::abort();
+            }
+
+#ifdef _WIN32
+            SetConsoleOutputCP(CP_UTF8);
+            SetConsoleCP(CP_UTF8);
 #endif
 
             // 终端回显日志消息
@@ -42,14 +71,20 @@ namespace _Logging_ {
             console_sink->set_pattern(log_fmt);
 
             // 写入外部文件的日志消息
+            std::string log_filename = fmt::format("{}/{}.log", log_dir, file_name);
             std::shared_ptr<spdlog::sinks::daily_file_sink_mt> file_sink =
-                std::make_shared<spdlog::sinks::daily_file_sink_mt>(fmt::format("{}/logs/{}.log", work_path, file_name), 0, 0);
+                std::make_shared<spdlog::sinks::daily_file_sink_mt>(log_filename, 0, 0, false, 30);
             file_sink->set_pattern(log_fmt);
 
-            // 整合日志系统
+            // 创建异步记录器
             spdlog::sinks_init_list log_sinks_list = { console_sink, file_sink };
-            m_log = std::make_unique<spdlog::logger>("stupid-log", log_sinks_list);
+            m_log = std::make_shared<spdlog::async_logger>(
+                "app-logger",
+                log_sinks_list,
+                spdlog::thread_pool(),
+                spdlog::async_overflow_policy::block);
             m_log->set_level(spdlog::level::trace);
+            m_log->set_error_handler([](const std::string& msg) { std::cerr << "[*** Logger ERROR ***] " << msg << std::endl; });
 
 #ifdef _DEBUG
             console_sink->set_level(spdlog::level::debug);
@@ -64,25 +99,25 @@ namespace _Logging_ {
             }
 #endif
             m_log->flush_on(spdlog::level::trace);
+        }
 
-            SetConsoleOutputCP(CP_UTF8);
-            SetConsoleCP(CP_UTF8);
+        void ShutDown()
+        {
+            std::shared_lock<std::shared_mutex> lock(m_mutex);
+            if (m_log) {
+                m_log->flush();
+            }
         }
 
     private:
-        std::unique_ptr<spdlog::logger> m_log = nullptr;
+        std::shared_ptr<spdlog::async_logger> m_log;
+        mutable std::shared_mutex m_mutex;
+
+        friend class asst::SingletonHolder<Logger>;
     };
 }
 
-#ifdef _DEBUG
 #define LOG_INFO(...)  SPDLOG_LOGGER_CALL(_Logging_::Logger::get_instance().log(), spdlog::level::info, __VA_ARGS__)
 #define LOG_WARN(...)  SPDLOG_LOGGER_CALL(_Logging_::Logger::get_instance().log(), spdlog::level::warn, __VA_ARGS__)
 #define LOG_ERROR(...) SPDLOG_LOGGER_CALL(_Logging_::Logger::get_instance().log(), spdlog::level::err, __VA_ARGS__)
 #define LOG_DEBUG(...) SPDLOG_LOGGER_CALL(_Logging_::Logger::get_instance().log(), spdlog::level::debug, __VA_ARGS__)
-#else
-#define LOG_INFO(...)  _Logging_::Logger::get_instance().log()->info(__VA_ARGS__)
-#define LOG_WARN(...)  _Logging_::Logger::get_instance().log()->warn(__VA_ARGS__)
-#define LOG_ERROR(...) _Logging_::Logger::get_instance().log()->error(__VA_ARGS__)
-#define LOG_DEBUG(...) _Logging_::Logger::get_instance().log()->debug(__VA_ARGS__)
-#endif // _DEBUG
-
