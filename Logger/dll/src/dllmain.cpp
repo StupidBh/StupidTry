f532d074921230e830a5ff264bef1ea4ff97eaff
StupidBhh <stupidbhh@gmail.com>
2025-07-17 10:25:11 +0800

    增添注释信息，并将函数标记为 static

--- Logger/dll/src/dllmain.cpp
+++ Logger/dll/src/dllmain.cpp
@@ -1,6 +1,6 @@
-﻿#include "log/logger.hpp"
+﻿#include "log/logger.hpp" // 必须包含、用以生成 lib
 
-BOOL APIENTRY DllMain([[maybe_unused]] HMODULE hModule, DWORD ul_reason_for_call, [[maybe_unused]] LPVOID lpReserved)
+static BOOL APIENTRY DllMain([[maybe_unused]] HMODULE hModule, DWORD ul_reason_for_call, [[maybe_unused]] LPVOID lpReserved)
 {
     switch (ul_reason_for_call) {
     case DLL_PROCESS_ATTACH:
