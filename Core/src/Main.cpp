3a7df8c3b84bab019e67d11acbebeb457abe415f
StupidBhh <stupidbhh@gmail.com>
2025-08-14 19:25:02 +0800

    Update

--- Core/src/Main.cpp
+++ Core/src/Main.cpp
@@ -1,23 +1,13 @@
-﻿#include "log/logger.hpp"
-#include "SingletonData.hpp"
+﻿#include "SingletonData.hpp"
 
 #include "Function.h"
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
 {
-    SCOPED_TIMER("main-function");
+    SCOPED_TIMER("main");
     SINGLE_DATA.m_vm = ProcessArguments(argc, argv);
 
     CallCmd("ping www.baidu.com");
-
-    try {
-        LOG_INFO("Physical-Cores: {}", get_core_count(CoreType::Physical));
-        LOG_INFO("Logical-Cores: {}", get_core_count(CoreType::Logical));
-        LOG_INFO("Total-Cores: {}", get_core_count(CoreType::Total));
-    }
-    catch (const std::exception& e) {
-        std::cerr << "Error: " << e.what() << '\n';
-    }
 
     _Logging_::Logger::get_instance().ShutDown();
     return 0;
