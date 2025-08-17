b41a05804f63f8ecc72e0459cd176144172f63cb
StupidBh <stupidbhh@gmail.com>
2025-08-17 15:44:34 +0800

    Update

--- Core/src/Main.cpp
+++ Core/src/Main.cpp
@@ -1,15 +1,26 @@
 ﻿#include "SingletonData.hpp"
 
 #include "Function.h"
+#include "ThreadPool.hpp"
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
 {
     SCOPED_TIMER("main");
-    SINGLE_DATA.m_vm = ProcessArguments(argc, argv);
+    SINGLE_DATA.VM = ProcessArguments(argc, argv);
 
-    CallCmd("ping www.baidu.com");
+
+    utils::ThreadPool pool(CPU_NUM);
+    std::vector<std::function<void()>> tasks = {
+        []() { CallCmd("ping www.baidu.com"); },
+        []() { CallCmd("ping www.bing.com"); },
+        []() { CallCmd("ping www.bilibili.com"); },
+        []() { CallCmd("ping 127.0.0.1"); }
+    };
+    for (auto& task : tasks) {
+        pool.enqueue(task);
+    }
+    pool.wait_for_completion();
 
     _Logging_::Logger::get_instance().ShutDown();
     return 0;
 }
-
