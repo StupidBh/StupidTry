802861786ffa9e0a931dd354d0d14e7dfc9eeeaa
StupidBhh <stupidbhh@gmail.com>
2025-08-18 21:00:02 +0800

    Update

--- Core/src/Main.cpp
+++ Core/src/Main.cpp
@@ -8,14 +8,13 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
     SCOPED_TIMER("main");
     SINGLE_DATA.VM = ProcessArguments(argc, argv);
 
-
     utils::ThreadPool pool(CPU_NUM);
-    std::vector<std::function<void()>> tasks = {
-        []() { CallCmd("ping www.baidu.com"); },
-        []() { CallCmd("ping www.bing.com"); },
-        []() { CallCmd("ping www.bilibili.com"); },
-        []() { CallCmd("ping 127.0.0.1"); }
-    };
+    std::vector<std::function<void()>> tasks = { []() { CallCmd("ping www.baidu.com", true); },
+                                                 []() { CallCmd("ping www.bing.com", true); },
+                                                 []() { CallCmd("ping www.bilibili.com", true); },
+                                                 []() {
+                                                     CallCmd("ping 127.0.0.1", false);
+                                                 } };
     for (auto& task : tasks) {
         pool.enqueue(task);
     }
