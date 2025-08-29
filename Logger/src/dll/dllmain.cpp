f8be8dd2fd73f7a8db1f1f92be29c105767bf5c1
StupidBhh <stupidbhh@gmail.com>
2025-08-29 20:22:57 +0800

    调整项目结构

--- Logger/src/dll/dllmain.cpp
+++ Logger/src/dll/dllmain.cpp
@@ -1,9 +1,9 @@
-﻿#include "log/logger.hpp" // 必须包含、用以生成 lib
+﻿#include "log/logger.hpp"
 
 BOOL APIENTRY DllMain([[maybe_unused]] HMODULE hModule, DWORD ul_reason_for_call, [[maybe_unused]] LPVOID lpReserved)
 {
     switch (ul_reason_for_call) {
-    case DLL_PROCESS_ATTACH:
+    case DLL_PROCESS_ATTACH: 
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH: break;
