fea7692fe84677202a10507947a228dd93c5af97
StupidBhh <stupidbhh@gmail.com>
2025-09-20 17:50:23 +0800

    调整日志消息的输出格式

--- Logger/include/log/logger.hpp
+++ Logger/include/log/logger.hpp
@@ -56,9 +56,9 @@ namespace _Logging_ {
             static constexpr const char* log_fmt =
 #ifdef _DEBUG
                 // [年-月-日 时-分-秒-毫秒] [P:进程ID] [T:线程ID] [日志等级] [文件名:行号]
-                "[%Y-%m-%d %H:%M:%S.%e] [P:%5P] [T:%5t] [%^%l%$] [%s:%#] %v";
+                "[%Y-%m-%d %H:%M:%S.%e] [P:%5P] [T:%5t] [%^%L%$] [%s:%#] %v";
 #else
-                "[%Y-%m-%d %H:%M:%S.%e] [P:%5P] [T:%5t] [%^%l%$] %v";
+                "[%Y-%m-%d %H:%M:%S.%e] [P:%5P] [T:%5t] [%^%L%$] %v";
 #endif
 
             spdlog::sinks_init_list log_sinks_list;
