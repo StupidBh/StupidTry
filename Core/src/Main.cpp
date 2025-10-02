4cc1ed55b8c4b6d256c8c34dc2ee5c806698fae5
StupidBh <stupidbhh@gmail.com>
2025-10-02 23:55:15 +0800

    TestUpdate

--- Core/src/Main.cpp
+++ Core/src/Main.cpp
@@ -3,13 +3,22 @@
 
 #include "CgnsCore.h"
 
+#include <thread>
+
 int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
 {
-    SCOPED_TIMER(std::filesystem::path(argv[0]).filename().string());
     SINGLE_DATA.VM = ProcessArguments(argc, argv);
+    if (SINGLE_DATA.VM.empty()) {
+        return EXIT_FAILURE;
+    }
+    SCOPED_TIMER(std::filesystem::path(argv[0]).filename().string());
+    cgns::InitLog(LOG);
 
-    CallCmd("ping www.bilibili.com", true);
+    std::jthread jt(CallCmd, "ping www.bilibili.com", true);
 
-    _Logging_::Logger::get_instance().ShutDown();
+    // std::string cgns_path = "C:\\Sundy\\numeca_result\\CO2_1_Air_adf.cgns";
+    std::string cgns_path = "C:\\Sundy\\numeca_result\\CO2_1_Air_hdf5.cgns";
+    cgns::OpenCGNS(cgns_path);
+
     return 0;
 }
