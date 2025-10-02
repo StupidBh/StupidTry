49eab87e2452909d3d9280f69632dacf09ec3776
StupidBh <stupidbhh@gmail.com>
2025-10-02 23:52:03 +0800

    移除无用代码及优化 DllMain 函数定义
    移除了文件开头的 UTF-8 BOM 标记。
    删除了无用的头文件引用 `CgnsCore.h` 和 `log/logger.hpp`。
    保留并调整了 `CGNSReadFramework.h` 的位置。
    优化了 `DllMain` 函数定义，添加 `[[maybe_unused]]` 属性以避免未使用参数的警告。
    删除了 `Test` 函数及其相关实现代码。

--- CGNSRead/src/dllmain.cpp
+++ CGNSRead/src/dllmain.cpp
@@ -1,8 +1,4 @@
-﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
-#include "CGNSReadFramework.h"
-
-#include "CgnsCore.h"
-#include "log/logger.hpp"
+﻿#include "CGNSReadFramework.h"
 
 BOOL APIENTRY DllMain([[maybe_unused]] HMODULE hModule, DWORD ul_reason_for_call, [[maybe_unused]] LPVOID lpReserved)
 {
@@ -13,16 +9,4 @@ BOOL APIENTRY DllMain([[maybe_unused]] HMODULE hModule, DWORD ul_reason_for_call
     case DLL_PROCESS_DETACH: break;
     }
     return TRUE;
-}
-
-void Test()
-{
-    int cg_file_id = -1;
-    cg_open("D:\\SomeThingCode\\tutorial1-adv.cgns", CG_MODE_READ, &cg_file_id);
-
-    float cg_file_version = 0.F;
-    cg_version(cg_file_id, &cg_file_version);
-
-    LOG_INFO("CGNS-v{:.2f}", cg_file_version);
-
 }
