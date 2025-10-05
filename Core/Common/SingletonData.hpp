94ee7394353381b0a5191d88beb5085bf426edd8
StupidBh <stupidbhh@gmail.com>
2025-10-06 00:25:16 +0800

    重构版本信息定义并新增依赖头文件
    重构了版本信息的定义，移除了旧的 `APP_VERSION`，新增了主版本号、次版本号和补丁号的静态常量，并通过 `std::format` 生成完整的版本号字符串 `STUPID_VERSION`。
    同时，新增了 `<string>`、`<format>` 和 `boost/program_options.hpp` 头文件的引用以支持新功能。

--- Core/Common/SingletonData.hpp
+++ Core/Common/SingletonData.hpp
@@ -1,11 +1,19 @@
 ﻿#pragma once
 #include "SingletonHolder.hpp"
 
+#include <string>
+#include <format>
+
 #include "boost/program_options.hpp"
 
 namespace stupid {
-    inline static constexpr const char* APP_NAME = "stupid-app";
-    inline static constexpr const char* APP_VERSION = "0.2.0";
+    static constexpr const char* APP_NAME = "stupid-app";
+
+    static constexpr int STUPID_VER_MAJOR = 1;
+    static constexpr int STUPID_VER_MINOR = 2;
+    static constexpr int STUPID_VER_PATCH = 2;
+    static const std::string STUPID_VERSION =
+        std::format("{}.{}.{}", STUPID_VER_MAJOR, STUPID_VER_MINOR, STUPID_VER_PATCH);
 
     class SingletonData final : public utils::SingletonHolder<SingletonData> {
         SINGLETON_CLASS(SingletonData);
