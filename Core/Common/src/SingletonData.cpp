f001d23b2a1209c60567bd4d516680a2aab31cef
StupidBhh <stupidbhh@gmail.com>
2026-01-16 17:25:24 +0800

    更新 SingletonData.cpp 中的命名空间和选项
    - 将 boost::program_options 的命名空间从 po 更改为 bpo。
    - 移除 workDirectory 的 required 属性，并将默认值设置为 "."。
    - 在命令行解析器中添加 allow_unregistered() 方法。
    - 更改异常处理，捕获 bpo::error。
    - 在日志初始化时直接使用 m_vm 中的值，简化代码。

--- Core/Common/src/SingletonData.cpp
+++ Core/Common/src/SingletonData.cpp
@@ -6,29 +6,27 @@
 
 void stupid::SingletonData::ProcessArguments(int argc, char* argv[])
 {
-    namespace po = boost::program_options;
+    namespace bpo = boost::program_options;
 
-    po::options_description desc("Usage: [options]", 150, 10);
-    desc.add_options()("help,h", "Display this help message")                                         //
-        ("inputPath,i", po::value<std::string>(), "Path to the input file")                           //
-        ("workDirectory,w", po::value<std::string>()->required(), "Directory for working (required)") //
-        ("cpuNum,n", po::value<int>()->default_value(2), "Number of CPU cores to use (default: 2)")   //
-        ("DEBUG", po::bool_switch()->default_value(false), "Enable verbose output")                   //
+    bpo::options_description desc("Usage: [options]", 150, 10);
+    desc.add_options()("help,h", "Display this help message")                                       //
+        ("inputPath,i", bpo::value<std::string>()->required(), "Path to the input file")            //
+        ("workDirectory,w", bpo::value<std::string>()->default_value("."), "Directory for working") //
+        ("DEBUG", bpo::bool_switch()->default_value(false), "Enable verbose output")                //
         ;
-
     std::ostringstream oss;
     oss << desc;
 
     try {
-        po::store(po::command_line_parser(argc, argv).options(desc).run(), this->m_vm);
+        bpo::store(bpo::command_line_parser(argc, argv).options(desc).allow_unregistered().run(), this->m_vm);
         if (this->m_vm.contains("help")) {
             std::cerr << oss.str() << std::endl;
             return;
         }
 
-        po::notify(this->m_vm);
+        bpo::notify(this->m_vm);
     }
-    catch (const po::error& e) {
+    catch (const bpo::error& e) {
         std::cerr << "Parameter error: " << e.what() << std::endl;
         std::cerr << oss.str() << std::endl;
         return;
@@ -43,8 +41,10 @@ void stupid::SingletonData::ProcessArguments(int argc, char* argv[])
     this->m_vm.at("DEBUG").value() = true;
 #endif
 
-    std::string workDirectory = this->m_vm["workDirectory"].as<std::string>();
-    dylog::Logger::get_instance().InitLog(workDirectory, stupid::APP_NAME, this->m_vm["DEBUG"].as<bool>());
+    dylog::Logger::get_instance().InitLog(
+        this->m_vm["workDirectory"].as<std::string>(),
+        stupid::APP_NAME,
+        this->m_vm["DEBUG"].as<bool>());
 }
 
 const boost::program_options::variables_map& stupid::SingletonData::get_variables_map() const noexcept
