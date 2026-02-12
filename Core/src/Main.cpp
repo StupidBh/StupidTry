fb1bb6e9ac9210ddb982285c3afe01f7f8ed8c1c
StupidBhh <stupidbhh@gmail.com>
2026-02-12 11:54:13 +0800

    refactor: Implementing the code for location movement

--- Core/src/Main.cpp
+++ Core/src/Main.cpp
@@ -3,19 +3,7 @@
 
 #include "highfive/highfive.hpp"
 
-template<utils::VectorType ValueType, class _Ty>
-requires std::same_as<std::remove_cvref_t<_Ty>, HighFive::Group>
-HighFive::DataSet WriteDataSet(const std::string& name, ValueType&& data, _Ty&& loc)
-{
-    using RawVectorType = std::remove_cvref_t<ValueType>;
-    using TrueValueType = typename RawVectorType::value_type;
-
-    HighFive::DataSet data_set = loc.createDataSet<TrueValueType>(name, HighFive::DataSpace::From(data));
-    data_set.write(std::forward<ValueType>(data));
-    return data_set;
-}
-
-int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
+int main(int argc, char* argv[])
 {
     SINGLE_DATA.ProcessArguments(argc, argv);
     if (SINGLE_DATA_VM.empty()) {
