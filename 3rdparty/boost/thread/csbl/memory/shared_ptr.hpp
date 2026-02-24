d6e3444afce620c96576d89f78e194f9802ef7bc
StupidBhh <stupidbhh@gmail.com>
2026-02-24 09:33:24 +0800

    Update 3rdparty and git settings

---
+++ 3rdparty/boost/thread/csbl/memory/shared_ptr.hpp
@@ -0,0 +1,21 @@
+// Copyright (C) 2014 Vicente J. Botet Escriba
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// 2014/10 Vicente J. Botet Escriba
+//   Creation.
+
+#ifndef BOOST_CSBL_MEMORY_SHARED_PTR_HPP
+#define BOOST_CSBL_MEMORY_SHARED_PTR_HPP
+
+#include <boost/thread/csbl/memory/config.hpp>
+
+namespace boost {
+    namespace csbl {
+        using std::make_shared;
+        using std::shared_ptr;
+    }
+}
+
+#endif // header
