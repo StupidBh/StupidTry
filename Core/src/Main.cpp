9e5b952a00e57e790933ab546e7b9ea5fdd7a04f
StupidBhh <stupidbhh@gmail.com>
2026-02-28 14:02:01 +0800

    feat: Test case for HDF5+HighFive

--- Core/src/Main.cpp
+++ Core/src/Main.cpp
@@ -2,6 +2,7 @@
 #include "SingletonData.h"
 
 #include "CgnsCore.h"
+#include "HighFive.hpp"
 
 int main(int argc, char* argv[])
 {
@@ -13,6 +14,15 @@ int main(int argc, char* argv[])
 
     cgns::InitLog(LOG);
     cgns::OpenCGNS(INPUT_PATH);
+
+    std::vector<float> output_data(1'000'000'000, 2.F);
+    const std::string H5_FILE = "./Try.h5";
+    HighFive::File file(H5_FILE, HighFive::File::Overwrite);
+    {
+        SCOPED_TIMER("WriteDataSet");
+        WriteDataSet("Try", output_data, file);
+        WriteDataSet("Try", std::move(output_data), file.createGroup("TryGroup"));
+    }
 
     spdlog::shutdown();
     return 0;
