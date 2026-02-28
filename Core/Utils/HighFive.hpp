2b782a382be679ec2273d8843cf61bc2e6d00534
StupidBhh <stupidbhh@gmail.com>
2026-02-28 13:55:39 +0800

    feat: 更新 HighFive.hpp 以支持不同版本的 HighFive
    在 `HighFive.hpp` 文件中，添加了对 `hdf5/H5Cpp.h` 的包含。修改了 `WriteDataSet` 函数的模板约束，使其支持 `HighFive::NodeTraits` 的派生类。同时，调整了函数实现，以适应不同版本的 HighFive，并引入了数据集的分块写入功能，以提高性能。

--- Core/Utils/HighFive.hpp
+++ Core/Utils/HighFive.hpp
@@ -1,15 +1,40 @@
 #pragma once
 #include "Utils.hpp"
+
+#include "hdf5/H5Cpp.h"
 #include "highfive/highfive.hpp"
 
 template<utils::VectorType ValueType, class _Ty>
-requires std::same_as<std::remove_cvref_t<_Ty>, HighFive::Group>
-HighFive::DataSet WriteDataSet(const std::string& name, ValueType&& data, _Ty&& loc)
+requires std::derived_from<std::remove_cvref_t<_Ty>, HighFive::NodeTraits<std::remove_cvref_t<_Ty>>>
+HighFive::DataSet WriteDataSet(const std::string& name, ValueType&& input, _Ty&& loc)
 {
     using RawVectorType = std::remove_cvref_t<ValueType>;
     using TrueValueType = typename RawVectorType::value_type;
 
-    HighFive::DataSet data_set = loc.createDataSet<TrueValueType>(name, HighFive::DataSpace::From(data));
-    data_set.write(std::forward<ValueType>(data));
+    HighFive::DataSet data_set;
+
+#if (HIGHFIVE_VERSION_MAJOR > 3) || (HIGHFIVE_VERSION_MAJOR == 3 && HIGHFIVE_VERSION_MINOR >= 3)
+
+    data_set = std::forward<_Ty>(loc).createDataSet<TrueValueType>(name, HighFive::DataSpace::From(input));
+    data_set.write(std::forward<ValueType>(input));
+
+#elif (HIGHFIVE_VERSION_MAJOR > 2) || (HIGHFIVE_VERSION_MAJOR == 2 && HIGHFIVE_VERSION_MINOR >= 10)
+
+    const std::size_t total_size = input.size();
+    const std::size_t chunk_size = std::clamp(total_size / 100, std::size_t(1024), std::size_t(1024 * 1024));
+
+    HighFive::DataSetCreateProps props;
+    props.add(HighFive::Chunking(std::vector<hsize_t> { chunk_size }));
+    H5Pset_fill_time(props.getId(), H5D_FILL_TIME_NEVER);
+
+    data_set = std::forward<_Ty>(loc).createDataSet<TrueValueType>(name, HighFive::DataSpace::From(input), props);
+    for (std::size_t offset = 0; offset < total_size; offset += chunk_size) {
+        std::size_t count = std::min(chunk_size, total_size - offset);
+        data_set.select({ offset }, { count }).write_raw(input.data() + offset);
+    }
+
+#else
+    #error "HighFive version is too old!"
+#endif
     return data_set;
 }
