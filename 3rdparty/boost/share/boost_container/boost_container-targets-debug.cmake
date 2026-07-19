4715d4bc249e807048a5f2af8e71ac8662fd0aac
StupidBh <stupidbhh@gmail.com>
2026-07-19 21:41:33 +0800

    build(boost): replace vendored GCC artifacts with MSVC

--- 3rdparty/boost/share/boost_container/boost_container-targets-debug.cmake
+++ 3rdparty/boost/share/boost_container/boost_container-targets-debug.cmake
@@ -8,12 +8,12 @@ set(CMAKE_IMPORT_FILE_VERSION 1)
 # Import target "Boost::container" for configuration "Debug"
 set_property(TARGET Boost::container APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
 set_target_properties(Boost::container PROPERTIES
-  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "C;CXX"
-  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/lib/libboost_container-gcc13-mt-d-x64-1_91.a"
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/debug/lib/boost_container-vc145-mt-gd-x64-1_91.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/bin/boost_container-vc145-mt-gd-x64-1_91.dll"
   )
 
 list(APPEND _cmake_import_check_targets Boost::container )
-list(APPEND _cmake_import_check_files_for_Boost::container "${_IMPORT_PREFIX}/debug/lib/libboost_container-gcc13-mt-d-x64-1_91.a" )
+list(APPEND _cmake_import_check_files_for_Boost::container "${_IMPORT_PREFIX}/debug/lib/boost_container-vc145-mt-gd-x64-1_91.lib" "${_IMPORT_PREFIX}/debug/bin/boost_container-vc145-mt-gd-x64-1_91.dll" )
 
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
--- 3rdparty/boost/share/boost_program_options/boost_program_options-targets-debug.cmake
+++ 3rdparty/boost/share/boost_program_options/boost_program_options-targets-debug.cmake
@@ -8,12 +8,12 @@ set(CMAKE_IMPORT_FILE_VERSION 1)
 # Import target "Boost::program_options" for configuration "Debug"
 set_property(TARGET Boost::program_options APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
 set_target_properties(Boost::program_options PROPERTIES
-  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
-  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/lib/libboost_program_options-gcc13-mt-d-x64-1_91.a"
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/debug/lib/boost_program_options-vc145-mt-gd-x64-1_91.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/bin/boost_program_options-vc145-mt-gd-x64-1_91.dll"
   )
 
 list(APPEND _cmake_import_check_targets Boost::program_options )
-list(APPEND _cmake_import_check_files_for_Boost::program_options "${_IMPORT_PREFIX}/debug/lib/libboost_program_options-gcc13-mt-d-x64-1_91.a" )
+list(APPEND _cmake_import_check_files_for_Boost::program_options "${_IMPORT_PREFIX}/debug/lib/boost_program_options-vc145-mt-gd-x64-1_91.lib" "${_IMPORT_PREFIX}/debug/bin/boost_program_options-vc145-mt-gd-x64-1_91.dll" )
 
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
