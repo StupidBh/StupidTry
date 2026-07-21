fe9bfea055817ac69dbdc92613e09f97f4568fbf
StupidBhh <stupidbhh@gmail.com>
2026-07-21 09:55:26 +0800

    feat(core): exercise variable-length HDF5 datasets

--- Core/src/Main.cpp
+++ Core/src/Main.cpp
@@ -4,6 +4,7 @@
 #include "ReaderCGNS/ReaderCGNS.h"
 #include "HighFiveUtils.hpp"
 #include "Utils/BlockingQueue.hpp"
+#include "mio/detail/string_util.hpp"
 
 void ReaderCGNSLogCallback(int level, const char* file, int line, const char* message)
 {
@@ -37,18 +38,15 @@ int main(int argc, char* argv[])
     ReaderCGNS::Logger::SetLogCallback(ReaderCGNSLogCallback);
     std::jthread test_thread([&] { ReaderCGNS::info(INPUT_PATH); });
 
+    const auto HF_FILE = WORK_DIR_PATH / "Try1.h5";
+
     struct Grid
     {
-        enum : std::size_t
-        {
-            DOC_CAPACITY = 64
-        };
-
-        std::uint32_t ID = 1;
-        float X = 1.F;
-        float Y = 1.F;
-        float Z = 1.F;
-        char doc[DOC_CAPACITY] = "TryToStringMessage";
+        std::uint32_t ID = 0;
+        float X = 0.F;
+        float Y = 0.F;
+        float Z = 0.F;
+        const char* doc = "NULL";
 
         static HighFive::CompoundType CompoundType()
         {
@@ -58,24 +56,55 @@ int main(int argc, char* argv[])
                     { "X", HighFive::create_datatype<float>(), offsetof(Grid, X) },           //
                     { "Y", HighFive::create_datatype<float>(), offsetof(Grid, Y) },           //
                     { "Z", HighFive::create_datatype<float>(), offsetof(Grid, Z) },           //
-                    { "DOC", HighFive::FixedLengthStringType(DOC_CAPACITY, HighFive::StringPadding::NullTerminated), offsetof(Grid, doc) },
+                    { "DOC", HighFive::VariableLengthStringType(), offsetof(Grid, doc) },     //
                 },
-                sizeof(Grid) //
-            );
+                sizeof(Grid));
         }
     };
 
-    std::vector<Grid> coordinates(1000);
-    std::vector<int> vec(1000);
-    const auto HF_FILE = WORK_DIR_PATH / "Try1.h5";
+    struct Coordinates : Grid
+    {
+        std::string doc_str;
+
+        explicit Coordinates(const Grid& grid) : Grid(grid)
+        {
+            doc_str = grid.doc;
+            doc = "NULL";
+        }
+    };
+
+    std::vector<Grid> coordinates;
 
     {
+        std::size_t VALUE_SUM = 100000;
+        std::vector<int> vec;
+        std::vector<std::string> grid_docs;
+        grid_docs.reserve(VALUE_SUM);
+        for (std::uint32_t i = 0; i < VALUE_SUM; ++i) {
+            float x = 1.F + static_cast<float>(i);
+            float y = 2.F + static_cast<float>(i);
+            float z = 3.F + static_cast<float>(i);
+            grid_docs.emplace_back(std::format("{}-[{},{},{}]", i, x, y, z));
+            vec.emplace_back(i);
+
+            coordinates.emplace_back(Grid { .ID = i,
+                                            .X = x,
+                                            .Y = y,
+                                            .Z = z, //
+                                            .doc = grid_docs.back().c_str() });
+        }
+
         SCOPED_TIMER_LOG(std::format("HighFive::Write file: [{}]", HF_FILE.string()));
-        HighFive::File file(HF_FILE.string(), HighFive::File::Overwrite);
+        HighFive::File file(HF_FILE.string(), HighFive::File::Truncate);
 
         try {
-            HFUtils::WriteDataSet(file, "Try1", coordinates, Grid::CompoundType());
-            HFUtils::WriteDataSet(file, "Try2", vec);
+            std::jthread task_1([&] { HFUtils::WriteDataSet(file, "Try1", vec); });
+            std::jthread task_2([&] { HFUtils::WriteDataSet(file, "Try2", coordinates, Grid::CompoundType()); });
+
+            std::jthread task_3([&] {
+                HFUtils::WriteDataSetAppendable(file, "Try3", coordinates, Grid::CompoundType());
+                HFUtils::WriteDataSetAppendable(file, "Try3", coordinates, Grid::CompoundType());
+            });
         }
         catch (const HighFive::Exception& e) {
             LOG_ERROR("Failed: {}", e.what());
@@ -83,24 +112,23 @@ int main(int argc, char* argv[])
         }
     }
 
-    {
+    { 
         utils::DeepClear(coordinates);
         SCOPED_TIMER_LOG("HighFive::Read file");
-
         HighFive::File file(HF_FILE.string(), HighFive::File::ReadOnly);
-        HighFive::DataSet dataset = file.getDataSet("Try1");
-        auto dataset_dims = dataset.getDimensions();
-        coordinates.resize(dataset_dims.front());
 
         try {
-            dataset.read_raw(coordinates.data(), Grid::CompoundType());
+            HFUtils::ReadDataSet(file.getDataSet("Try2"), coordinates, Grid::CompoundType());
         }
         catch (const HighFive::Exception& e) {
             LOG_ERROR("Failed: {}", e.what());
             return 1;
         }
+
+        for (std::size_t i = 0; i < std::min<std::size_t>(coordinates.size(), 10); ++i) {
+            LOG_INFO("DOC: {}", coordinates[i].doc);
+        }
     }
-    test_thread.join();
 
     {
         SCOPED_TIMER_LOG("ProducerConsumer demo");
@@ -145,6 +173,7 @@ int main(int argc, char* argv[])
         LOG_INFO("ProducerConsumer demo passed, consumed size={}", consumed.size());
     }
 
+    test_thread.join();
     ReaderCGNS::Logger::ClearLogCallback();
     return 0;
 }
