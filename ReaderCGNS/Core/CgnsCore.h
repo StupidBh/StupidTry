4e2d3a23015bedc4e362a2b2448d74cb119c3b1c
StupidBhh <stupidbhh@gmail.com>
2026-08-28 15:47:29 +0800

    refactor(ReaderCGNS): load reader through DLL exports

--- ReaderCGNS/Core/CgnsCore.h
+++ ReaderCGNS/Core/CgnsCore.h
@@ -1,27 +1,8 @@
 #pragma once
-#include "ReaderCGNS/ReaderCGNS.h"
+#include "FileManager.h"
 
-namespace ReaderCGNS {
-    class CgnsCore {
-    public:
-        CgnsCore() = default;
-        CgnsCore(const std::string& cgns_file_path);
-        ~CgnsCore();
-
-        CgnsCore(const CgnsCore&) = delete;
-        CgnsCore& operator=(const CgnsCore&) = delete;
-        CgnsCore(CgnsCore&&) = delete;
-        CgnsCore& operator=(CgnsCore&&) = delete;
-
-        [[nodiscard]] bool IsOpen() const;
-        bool OpenCGNS();
-        bool OpenCGNS(const std::string& cgns_file_path);
-        void CloseCGNS();
-
-    private:
-        void CloseFile();
-
-        std::string m_cgns_file_path;
-        int m_cg_file_id = 0;
-    };
-} // namespace ReaderCGNS
+class CgnsCore : virtual public FileManager {
+public:
+    bool info() override;
+    void* QueryInterface() override;
+};
