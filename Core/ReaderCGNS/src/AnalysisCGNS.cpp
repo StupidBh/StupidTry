3cc6eb2075753b59464475eb6141381b159ba564
StupidBhh <stupidbhh@gmail.com>
2026-09-10 15:26:28 +0800

    [update]

--- Core/ReaderCGNS/src/AnalysisCGNS.cpp
+++ Core/ReaderCGNS/src/AnalysisCGNS.cpp
@@ -79,27 +79,30 @@ bool AnalysisCGNS::Analyze(const std::string& cgns_file_path) const
         return false;
     }
 
-    this->m_reader->info();
+    // this->m_reader->info();
 
     LOG_INFO("[ReaderCGNS] solver type: {}", this->m_reader->GetSolverType());
+
+    std::vector<ReaderAPI::Elem> all_elements;
+    if (this->m_reader->GetAllElement(all_elements)) {
+        LOG_INFO("AllElement: {}", all_elements.size());
+    }
+
     std::vector<std::string> element_set_names;
     if (this->m_reader->GetAllElementSetName(element_set_names)) {
-        LOG_INFO("ElementSet: {}", element_set_names);
+        LOG_INFO("ElementSet: {}:{}", element_set_names, element_set_names.size());
     }
 
     std::vector<std::string> field_function_names;
     if (this->m_reader->GetAllFieldFunctionName(field_function_names)) {
         std::vector<ReaderAPI::Field> loaded_fields;
         if (this->m_reader->GetFieldFunctionData(field_function_names, loaded_fields)) {
+            LOG_INFO("FieldFunction: {}:{}", field_function_names, field_function_names.size());
+
             for (auto& [name, type, ids, values] : loaded_fields) {
                 LOG_INFO("FieldFunction: {}, type={}, ids={}, value={}", name, type, ids.size(), values.size());
             }
         }
-    }
-
-    std::vector<ReaderAPI::Elem> all_elements;
-    if (this->m_reader->GetAllElement(all_elements)) {
-        LOG_INFO("AllElement: {}", all_elements.size());
     }
 
     return true;
